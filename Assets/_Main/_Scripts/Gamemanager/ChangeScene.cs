f47b2c668e73f27e069888e523ee09ec00031edd
mbertolini29 <mbertolini29@users.noreply.github.com>
2024-06-23 17:38:01 -0300

    interfaz de inicio, menu, gameover y pausa

--- Assets/_Main/_Scripts/Gamemanager/ChangeScene.cs
+++ Assets/_Main/_Scripts/Gamemanager/ChangeScene.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] GameObject CreditOn;
+    [SerializeField] bool paused = false;
+
     public void PlayAgain()
     {
-        //AudioManager.instance.Play("Button");
+        AudioManager.instance.Play("Button");
         SceneManager.LoadScene(1);
         //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void Credit()
+    {
+        AudioManager.instance.Play("Button");
+        paused = !paused;
+        Time.timeScale = paused ? 0 : 1;
+        CreditOn.SetActive(paused);
+    }
+    
+    public void ContinueGame()
+    {
+        AudioManager.instance.Play("Button");
+        paused = !paused;
+        Time.timeScale = paused ? 0 : 1;
+        CreditOn.SetActive(paused);
+    }
+
     public void Exit()
     {
-        //AudioManager.instance.Play("Button");
+        AudioManager.instance.Play("Button");
         Application.Quit();
     }
 
